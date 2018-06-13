0cb5a33b8e1fc38b4ffab20a68fa2bda41376d98
Yoonmee <ymhwang119@naver.com>
2018-06-13 21:32:51 +0900

    tq bb

--- dropbox/app.js
+++ dropbox/app.js
@@ -1,23 +1,37 @@
 var createError = require('http-errors');
 var express = require('express');
+const http = require('http');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const db = require('./db.js');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const secret_key = crypto.randomBytes(48);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(session({
+    secret: secret_key.toString('hex'),
+    resave: false,
+    saveUninitialized: true
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
